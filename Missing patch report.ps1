cecb7c3950e764f2543c7ee7a959a6ab30bec8d0
Witt Allen <Wittionary@users.noreply.github.com>
2018-04-06 09:18:42 -0500

    Working state
    "It's working!"
    - Anakin Skywalker

--- Missing patch report.ps1
+++ Missing patch report.ps1
@@ -1,19 +1,36 @@
 Param(
     # One KB bulletin ID per line
     [Parameter(Mandatory = $True, Position = 0)]
-    [System.IO.FileSystemInfo]$kbList, 
+    [string]$listPath, 
     
     # If filtering, the organization's ID that you've defined in Kaseya (System > Orgs/Groups/Depts/Staff > Manage)
     [Parameter(Position = 1)]
     [string]$organizationID
 )
 
-$kbList = "C:\users\wallen\Desktop\patches.txt"
+$kbList = Get-Content -Path $listPath
+
+$reportName = "Patch report "
+$date = Get-Date -UFormat "%m-%d-%Y"
+
+if ($organizationID -ne $null) {
+    $reportName += "for " + $organizationID + " "
+} 
+$reportName += $date + ".csv"
 
 if (!(Test-Path "C:\temp\")) {
     New-Item "C:\temp\" -ItemType Directory
 }
+if (Test-Path "C:\temp\$reportName") {
+    Remove-Item -Path "C:\temp\$reportName" -Force
+}
 
+foreach ($kb in $kbList) { 
+    $sqlQuery = "select machname, groupname, displayName from machnametab where agentguid in (select distinct(agentguid) `
+                from patchstatus where bulletinId like '%"+ $kb + "%')"
+    Invoke-Sqlcmd -query $sqlQuery -Database "ksubscribers" | `
+        Where-Object {$_.groupname -Like "*$organizationID"} | `
+        Out-File -FilePath "C:\temp\$reportName" -Encoding default -Append
+}
 
-Invoke-Sqlcmd -query "select machname, groupname, displayName from machnametab where agentguid in (select distinct(agentguid) from patchstatus where bulletinId like '%KB4088877%')" -Database "ksubscribers"
-Invoke-SQL | Out-File "C:\machines.csv"
+Write-Host "$reportName is ready in C:\temp\"
